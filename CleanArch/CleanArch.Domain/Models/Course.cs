09e33889402722cddf868403403060c542a0f4e4
g0dgamerz <jideshvaidya@gmail.com>
2022-07-24 21:58:47 +0545

    added course domain model

--- CleanArch/CleanArch.Domain/Models/Course.cs
+++ CleanArch/CleanArch.Domain/Models/Course.cs
@@ -1,4 +1,10 @@
-﻿namespace CleanArch.Domain
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.Models
 {
     public class Course
     {
